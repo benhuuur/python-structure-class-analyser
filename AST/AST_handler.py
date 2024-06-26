f9deb729bc155ee9883602df8407718c372600cd
Benhur Feld <benhurfeld@gmail.com>
2024-06-26 10:57:14 -0300

    make documentation from AST_Handler

--- AST/AST_handler.py
+++ AST/AST_handler.py
@@ -9,16 +9,40 @@ from data_class.function_info import function_info
 class AST_handler:
     @staticmethod
     def get_AST(file_path):
+        """
+        Parses the given Python file and returns the abstract syntax tree (AST) representation.
+
+        Args:
+        - file_path (str): Path to the Python file.
+
+        Returns:
+        - ast.AST: Abstract syntax tree representation of the parsed Python file.
+        """
         with open(file_path, "r") as file:
             return ast.parse(file.read())
 
     @staticmethod
     def print_tree(AST_tree):
+        """
+        Pretty prints the AST tree structure.
+
+        Args:
+        - AST_tree (ast.AST): Abstract syntax tree representation of the Python code.
+        """
         print("\n")
         pprint(ast.dump(AST_tree))
 
     @staticmethod
     def get_classes(tree_body):
+        """
+        Extracts class information from the AST tree body.
+
+        Args:
+        - tree_body (list): List of AST nodes.
+
+        Returns:
+        - list: List of class_info objects representing the classes found in the AST.
+        """
         classes = list()
         for node in tree_body:
             if isinstance(node, ast.ClassDef):
@@ -37,6 +61,15 @@ class AST_handler:
 
     @staticmethod
     def get_functions(node_body):
+        """
+        Extracts function information from the given AST node body.
+
+        Args:
+        - node_body (list): List of AST nodes.
+
+        Returns:
+        - list: List of function_info objects representing the functions found in the node body.
+        """
         functions = list()
         for statement in node_body:
             if isinstance(statement, (ast.FunctionDef, ast.AsyncFunctionDef)):
@@ -45,12 +78,30 @@ class AST_handler:
 
     @staticmethod
     def _get_function(statement):
+        """
+        Extracts detailed information about a function from its AST representation.
+
+        Args:
+        - statement (ast.FunctionDef or ast.AsyncFunctionDef): AST node representing the function.
+
+        Returns:
+        - function_info: Object containing details of the function.
+        """
         function_name = statement.name
         arguments = [arg.arg for arg in statement.args.args]
         return function_info(name=function_name, args=arguments, return_value=None)
 
     @staticmethod
     def get_assignments(node_body):
+        """
+        Extracts assignment information from the given AST node body.
+
+        Args:
+        - node_body (list): List of AST nodes.
+
+        Returns:
+        - list: List of assignments_info objects representing the assignments found in the node body.
+        """
         assignments = list()
         for statement in node_body:
             if isinstance(statement, ast.Assign):
@@ -60,6 +111,15 @@ class AST_handler:
 
     @staticmethod
     def _get_assignment(statement):
+        """
+        Extracts detailed information about an assignment statement from its AST representation.
+
+        Args:
+        - statement (ast.Assign): AST node representing the assignment statement.
+
+        Returns:
+        - list: List of assignments_info objects representing each assignment target found in the statement.
+        """
         assignment_names = list()
         for target in statement.targets:
             if isinstance(target, ast.Name):
@@ -76,7 +136,8 @@ class AST_handler:
 
         assignment_data_type = list()
         if isinstance(statement.value, ast.Constant):
-            assignment_data_type.append(f"{type(statement.value.value).__name__}")
+            assignment_data_type.append(
+                f"{type(statement.value.value).__name__}")
         else:
             assignment_data_type.append("None")
 
@@ -86,6 +147,15 @@ class AST_handler:
 
     @staticmethod
     def _get_init_assignment(node_body):
+        """
+        Extracts assignment information specifically from an __init__ method in a class.
+
+        Args:
+        - node_body (list): List of AST nodes representing the body of a class or function.
+
+        Returns:
+        - list: List of assignments_info objects representing the assignments found in the __init__ method.
+        """
         for statement in node_body:
             if isinstance(statement, (ast.FunctionDef, ast.AsyncFunctionDef)) and statement.name == "__init__":
                 return AST_handler.get_assignments(statement.body)
